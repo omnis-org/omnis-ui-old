e228a37917fdf2a28eac77afe2a360096bdcf458
mudcam <v.nieutin@live.fr>
2020-11-22 12:36:15 +0100

    delete default angular test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,11 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('omnis-ui');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('omnis-ui app is running!');
-  });
 });
