f869ef3d9e53a82abe856338cde566fdcb46b9de
mudcam <v.nieutin@live.fr>
2020-12-04 15:19:39 +0100

    fix interface name

--- src/app/objects/interface.ts
+++ src/app/objects/interface.ts
@@ -1,10 +1,10 @@
-export interface Network {
-    id: number;
-    name: string;
-    ipv4: string;
-    ipv4_mask: number;
-    mac: string;
-    interface_type: string;
-    machine_id: number;
-    network_id: number;
-  }
+export interface Interface {
+  id: number;
+  name: string;
+  ipv4: string;
+  ipv4_mask: number;
+  mac: string;
+  interface_type: string;
+  machine_id: number;
+  network_id: number;
+}
