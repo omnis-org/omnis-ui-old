ae0e31f7a24a5aaedbab5e04222a138ad81e19e9
Arthur Guyader <arthur.guyader@iot.bzh>
2020-12-12 15:38:19 +0100

    Add test
    Change-Id: I29005ad6b3803124065714745734aabbad5081cd

--- src/app/machine-details/machine-details.component.spec.ts
+++ src/app/machine-details/machine-details.component.spec.ts
@@ -1,23 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { MachineDetailsComponent } from './machine-details.component';
+import { MachineService, AlertService } from '@app/services';
+import { HttpClient } from '@angular/common/http';
+import { OmnisMachine } from '@app/models';
 
 describe('MachineDetailsComponent', () => {
   let component: MachineDetailsComponent;
   let fixture: ComponentFixture<MachineDetailsComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-      declarations: [ MachineDetailsComponent ]
+    TestBed.configureTestingModule({
+      declarations: [MachineDetailsComponent],
+      imports: [ReactiveFormsModule, HttpClientTestingModule],
+      providers: [MachineService, AlertService]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(MachineDetailsComponent);
     component = fixture.componentInstance;
+    component.machine = new OmnisMachine();
     fixture.detectChanges();
   });
 
