42eb4addd6070a5260b514949c33d97d71bf416d
Arthur Guyader <arthur@guyader.info>
2021-01-04 17:21:53 +0100

    Use pending machines
    Signed-off-by: Arthur Guyader <arthur@guyader.info>
    Change-Id: I78e42f70d69e0d2b71be46b03d364fadc683621e

---
+++ src/app/admin/pending-machines/list-pending-machines.component.ts
@@ -0,0 +1,52 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs/operators';
+
+import { MachineService, AlertService, AccountService } from '@app/services';
+
+@Component({ templateUrl: 'list-pending-machines.component.html' })
+export class ListPendingMachinesComponent implements OnInit {
+    pendingMachines = null;
+
+    constructor(public accountService: AccountService, private machineService: MachineService, private alertService: AlertService) { }
+
+    ngOnInit() {
+        // only with activated when implemented
+        this.machineService.getPendingMachines()
+            .pipe(first())
+            .subscribe(pendingMachines => this.pendingMachines = pendingMachines);
+    }
+
+    authorizeMachine(id: string) {
+        const machine = this.pendingMachines.find(x => x.id === id);
+        machine.isAuthorize = true;
+        this.machineService.authorize(id)
+            .subscribe({
+                next: () => {
+                    this.pendingMachines = this.pendingMachines.filter(x => x.id !== id);
+                    this.alertService.success('Authorized successful');
+                    machine.isAuthorize = false;
+                },
+                error: error => {
+                    this.alertService.error(error);
+                    machine.isAuthorize = false;
+                }
+            });
+    }
+
+    unauthorizeMachine(id: string) {
+        const machine = this.pendingMachines.find(x => x.id === id);
+        machine.isUnauthorize = true;
+        this.machineService.unauthorize(id)
+            .subscribe({
+                next: () => {
+                    this.pendingMachines = this.pendingMachines.filter(x => x.id !== id);
+                    this.alertService.success('Unauthorized successful');
+                    machine.isUnauthorize = false;
+                },
+                error: error => {
+                    this.alertService.error(error);
+                    machine.isUnauthorize = false;
+                }
+            });
+    }
+}
