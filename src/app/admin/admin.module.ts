cf8d551f0c2278772a752ac6d3b2f9c17cc70308
Arthur Guyader <arthur@guyader.info>
2021-01-04 17:22:53 +0100

    Add paths and dependencies
    Signed-off-by: Arthur Guyader <arthur@guyader.info>
    Change-Id: I49d841789f0539b88d821b04c63e53f5504c67b0

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,22 +1,32 @@
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { LayoutComponent } from './layout.component';
-import { ListComponent } from './list.component';
-import { AddEditComponent } from './add-edit.component';
+import { ListUsersComponent } from './users/list-users.component';
+import { ListPendingMachinesComponent } from './pending-machines/list-pending-machines.component';
+import { AddEditUserComponent } from './users/add-edit-user.component';
+import { ListRolesComponent } from './roles/list-roles.component';
+import { AddEditRoleComponent } from './roles/add-edit-role.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { PermissionsComponent } from './roles/permissions.component';
 
 @NgModule({
     imports: [
         CommonModule,
+        FormsModule,
         ReactiveFormsModule,
         AdminRoutingModule
     ],
     declarations: [
         LayoutComponent,
-        ListComponent,
-        AddEditComponent
+        ListRolesComponent,
+        ListPendingMachinesComponent,
+        ListUsersComponent,
+        AddEditUserComponent,
+        AddEditRoleComponent,
+        PermissionsComponent
     ]
 })
 export class AdminModule { }
