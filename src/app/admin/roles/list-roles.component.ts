67c35bbdae0a78b4ebedc86321da17470863de29
Arthur Guyader <arthur@guyader.info>
2021-01-04 17:20:53 +0100

    Use role and permissions
    Signed-off-by: Arthur Guyader <arthur@guyader.info>
    Change-Id: Ie189b114fa21af6ba401517b044a150a5a14f2ed

---
+++ src/app/admin/roles/list-roles.component.ts
@@ -0,0 +1,38 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs/operators';
+
+import { RoleService, AlertService, AccountService } from '@app/services';
+
+@Component({
+    templateUrl: 'list-roles.component.html',
+    styleUrls: ['./list-roles.component.scss']
+})
+export class ListRolesComponent implements OnInit {
+    roles = null;
+
+    constructor(public accountService: AccountService,
+        private roleService: RoleService,
+        private alertService: AlertService) { }
+
+    ngOnInit() {
+        this.roleService.getAll().subscribe(roles => this.roles = roles);
+    }
+
+    deleteRole(id: string) {
+        const user = this.roles.find(x => x.id === id);
+        user.isDeleting = true;
+        this.roleService.delete(id)
+            .subscribe({
+                next: () => {
+                    this.roles = this.roles.filter(x => x.id !== id);
+                    this.alertService.success('Delete successful');
+                    user.isDeleting = false;
+                },
+                error: error => {
+                    this.alertService.error(error);
+                    user.isDeleting = false;
+                }
+            });
+    }
+
+}
---
+++ src/app/services/admin/role.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { Role } from '@app/models';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import { environment } from '@environments/environment';
+@Injectable({
+  providedIn: 'root'
+})
+export class RoleService {
+  public roles: Observable<Role[]>;
+  private rolesSubject: BehaviorSubject<Role[]>;
+
+  private httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+    responseType: 'text/plain' as 'json'
+  };
+
+  constructor(private http: HttpClient) { }
+
+  public get rolesValue(): Role[] {
+    return this.rolesSubject.value;
+  }
+
+  getAll() {
+    return this.http.get<Role[]>(`${environment.adminApiUrl}/roles`);
+  }
+
+  getById(id: number | string) {
+    return this.http.get<Role>(`${environment.adminApiUrl}/role/${id}`);
+  }
+
+  update(role: Role) {
+    return this.http.put<any>(`${environment.adminApiUrl}/role/${role.id}`, role, this.httpOptions);
+  }
+
+  insert(role: Role) {
+    return this.http.post<any>(`${environment.adminApiUrl}/role`, role, this.httpOptions);
+  }
+
+  delete(id: string | number) {
+    return this.http.delete<any>(`${environment.adminApiUrl}/role/${id}`, this.httpOptions);
+  }
+}
